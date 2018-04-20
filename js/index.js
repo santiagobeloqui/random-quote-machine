489b4f6144f2952e3e369289199a2c6a28a82cc2
santiagobeloqui <sabeloqui@gmail.com>
2018-04-19 23:01:55 -0300

    totalmente funcional

--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let quoteButton = document.getElementById("changeQuote");
 let tweetButton = document.getElementById("tweet");
+let linkTwitter = document.getElementById("linkTwitter");
 let body = document.getElementById("body");
 
 let currentColor;
 let currentQuote;
+
+
+
 
 function change(){
 	let randomQuote = Math.floor(Math.random()*103);
@@ -21,12 +25,17 @@ function change(){
 	body.style.backgroundColor = colors[randomColor];
 	quoteButton.style.backgroundColor = colors[randomColor];
 	tweetButton.style.backgroundColor = colors[randomColor];
+	linkTwitter.setAttribute("href", `https://twitter.com/home/?status="${quotes[randomQuote]["quote"]}", ${quotes[randomQuote]["name"]}.`);
 	currentColor = randomColor;
 	currentQuote = randomQuote;
+
 }
+
 
 window.onload = change;
 quoteButton.addEventListener("click", change);
+
+
 
 let colors = ["#16A086", "#27AE61", "#297FB8", "#8D44AF", "#2D3E50", "#F39C11", "#D25400", "#C1392B"];
 
